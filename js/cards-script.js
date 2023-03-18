73434a714206714b0ab429de78c7427caa6dbc58
Jesto K J <76833104+Jes-Black@users.noreply.github.com>
2023-03-18 08:55:29 +0530

    Add files via upload

---
+++ js/cards-script.js
@@ -0,0 +1,38 @@
+var swiper = new Swiper(".slide-container", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    sliderPerGroup: 3,
+    loop: true,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+      delay: 2000,
+    },
+  
+  
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1000: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
